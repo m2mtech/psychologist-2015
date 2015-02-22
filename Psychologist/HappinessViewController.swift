67ed42cd1507e0c2c085fed04cf497ea1b91da66
m2m tech <tech@m2m.at>
2015-02-22 20:45:42 +0100

    end of lecture #8

--- Psychologist/HappinessViewController.swift
+++ Psychologist/HappinessViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
+var globalHappinessInstanceCount = 0
+
 class HappinessViewController: UIViewController, FaceViewDataSource
 {
+    var instanceCount = { globalHappinessInstanceCount++ }()
+
     var happiness: Int = 100 { // 0 = very sad, 100 = ecstatic
         didSet {
             happiness = min(max(happiness, 0), 100)
-            println("happiness = \(happiness)")
+            logVCL("prepared by prepareForSegue (happiness = \(happiness))")
             updateUI()
         }
     }
@@ -54,4 +58,65 @@ class HappinessViewController: UIViewController, FaceViewDataSource
         return Double(happiness - 50) / 50.0
     }
 
+    func logVCL(msg: String) {
+        println(logVCLprefix + "Happiness \(instanceCount) " + msg)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        logVCL("init(coder)")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        logVCL("init(nibName, bundle)")
+    }
+    
+    deinit {
+        logVCL("deinit")
+    }
+    
+    override func awakeFromNib() {
+        logVCL("awakeFromNib()")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logVCL("viewDidLoad()")
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        logVCL("viewWillAppear(animated = \(animated))")
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        logVCL("viewDidAppear(animated = \(animated))")
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        logVCL("viewWillDisappear(animated = \(animated))")
+    }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        logVCL("viewDidDisappear(animated = \(animated))")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        logVCL("viewWillLayoutSubviews() bounds.size = \(view.bounds.size)")
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        logVCL("viewDidLayoutSubviews() bounds.size = \(view.bounds.size)")
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        logVCL("viewWillTransitionToSize")
+        coordinator.animateAlongsideTransition({ (context: UIViewControllerTransitionCoordinatorContext!) -> Void in
+            self.logVCL("animatingAlongsideTransition")
+            }, completion: { context -> Void in
+                self.logVCL("doneAnimatingAlongsideTransition")
+        })
+    }
 }
--- README.md
+++ README.md
@@ -5,5 +5,7 @@ written in Xcode 6.1.1 for iOS8.1+
 
 ### various versions are available via branches and tags:
 
+[End of Lecture #8](http://cs193p.m2m.at/cs193p-lecture-8-view-controller-lifecycle-autolayout-winter-2015/) -> [EndOfLecture8](https://github.com/m2mtech/psychologist-2015/tree/EndOfLecture8)
+
 [End of Lecture #7](http://cs193p.m2m.at/cs193p-lecture-7-multiple-mvcs-winter-2015/) -> [EndOfLecture7](https://github.com/m2mtech/psychologist-2015/tree/EndOfLecture7)
 
